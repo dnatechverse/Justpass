b9ce271697960e9b789333a046cacce1bfee0396
Thinakaran <thinakaranmanohar@yahoo.com>
2025-04-25 23:41:03 +0530

    Published

--- client/src/utils/public/ENVConfig.js
+++ client/src/utils/public/ENVConfig.js
@@ -1,3 +1,3 @@
-const API_URL = import.meta.env.API_URL || 'http://localhost:5000'; 
+const API_URL = import.meta.env.API_URL; 
 
 export default API_URL; // Export the API_URL for use in other parts of the application
