4b075d36f7ddf8d4986fc7895ca80281aa283650
Thinakaran <thinakaranmanohar@yahoo.com>
2025-04-29 00:35:43 +0530

    Issue solved #1

--- client/src/pages/user/Courses.jsx
+++ client/src/pages/user/Courses.jsx
@@ -47,36 +47,37 @@ const Courses = () => {
                             matchedCourses.length > 0 ? (
                                 <div className="w-full ">
                                     {matchedCourses.map((course) => (
-                                        <div key={course._id} className=" w-full flex space-x-4">
-                                            <div className='w-1/2' >
-                                                <img src={course.img} alt={course.title} className="w-full object-cover mb-4 max-h-96 rounded-3xl" />
+                                        <div key={course._id} className=" w-full md:flex md:space-x-4">
+                                            <div className='w-full md:w-1/2 mt-4 md:mt-0 ' >
+                                                <div>
+                                                    <img src={course.img} className=' rounded-4xl  ' alt="" srcset="" />
+                                                </div>
                                             </div>
-                                            <div className="w-1/2 py-6 px-4 mb-4">
-                                                <div className='space-y-4 ' >
-                                                    <h1 className='font-unboundedbold text-4xl  ' >{course.title}</h1>
-                                                    <p className='text-lg font-generalregular  text-gray-600 ' >{course.description}</p>
+                                            <div className='w-full md:w-1/2 p-2 md:p-6 space-y-4 md:space-y-6' >
+                                                <div className='space-y-2 md:space-y-4 ' >
+                                                    <h1 className='font-unboundedbold text-2xl md:text-4xl  ' >{course.title}</h1>
+                                                    <p className='text-sm md:text-lg font-generalregular  text-gray-600 ' >{course.description}</p>
                                                     <div className='flex space-x-4 font-generalregular ' >
-                                                        <div className='flex space-x-1 items-center bg-black text-white px-4 py-2 rounded-2xl  ' >
+                                                        <div className='flex space-x-1 items-center bg-black text-white px-3 md:px-4 py-1.5 md:py-2 rounded-lg md:rounded-2xl text-sm md:text-base  ' >
                                                             <div><PiHandshakeFill /></div>
                                                             <div>{course.level}</div>
                                                         </div>
-                                                        <div className='flex space-x-1 items-center bg-linear-to-r from-pink-500 via-pink-600 to-red-500 hover:from-red-500 hover:via-pink-600 hover:to-pink-500 transition-colors duration-500 text-white px-4 py-2 rounded-2xl  ' >
+                                                        <div className='flex space-x-1 items-center bg-linear-to-r from-pink-500 via-pink-600 to-red-500 hover:from-red-500 hover:via-pink-600 hover:to-pink-500 px-3 transition-colors duration-500  md:px-4 py-1.5 md:py-2 rounded-lg md:rounded-2xl text-sm md:text-base text-white ' >
                                                             <div><PiClockBold /></div>
                                                             <div>{course.duration}</div>
                                                         </div>
                                                     </div>
                                                 </div>
                                                 <div className='w-full'>
-                                                    <div className='w-full space-y-4 mt-8' >
-                                                        <h3 className='font-authormedium text-2xl font-semibold  ' >Skills you're going to learn</h3>
-                                                        <div className=' flex flex-wrap w-full space-x-4 space-y-3 font-generalregular '>
+                                                    <div className='w-full space-y-4 mt-4 md:mt-8' >
+                                                        <h3 className='font-authormedium text-xl md:text-2xl font-semibold  ' >Skills you're going to learn</h3>
+                                                        <div className=' flex flex-wrap w-full space-x-2 md:space-x-4 space-y-3 font-generalregular '>
                                                             {course.skills.map((skill, index) => (
-                                                                <div className='bg-grey border-[1px] border-black  px-4 py-1.5 h-fit min-w-24 text-center rounded-2xl  ' key={index}>{skill}</div>
+                                                                <div className='bg-grey border-[1px] border-black  px-4 py-1.5 h-fit min-w-16 text-sm md:text-base md:min-w-24 text-center rounded-2xl  ' key={index}>{skill}</div>
                                                             ))}
                                                         </div>
                                                     </div>
                                                 </div>
-
                                             </div>
                                         </div>
                                     ))}
@@ -89,7 +90,7 @@ const Courses = () => {
                                         <div className='relative font-authormedium  w-full flex items-center flex-col   ' >
                                             <div className="text-lg mt-3 md:mt-12  text-black md:absolute z-10 md:max-w-2/5  mx-auto space-y-6">
                                                 <div>It looks like you haven't enrolled in any courses yet.
-                                                Explore our available courses and start your learning journey today!</div>
+                                                    Explore our available courses and start your learning journey today!</div>
                                                 <Link to="/" className="bg-black text-white px-6 py-4 rounded-2xl  transition duration-300 text-sm md:text-xl  hover:scale-105   ">Explore Courses</Link>
                                             </div>
                                         </div>
